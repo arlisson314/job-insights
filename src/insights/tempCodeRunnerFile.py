4431c8d5d32a07dd68006d699bba7fe02e727292
Arlisson N. Costa <arlisson314@gmail.com>
2022-12-12 16:04:11 -0500

    feat: exluir arquivo tempCodeRunneFile.py

--- src/insights/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-def get_unique_job_types(path: str) -> List[str]:
-    list_infos = read(path)
-    unique_job = list()
-    for job in list_infos:
-        if job not in unique_job:
-            unique_job.append(job)
-    return unique_job
