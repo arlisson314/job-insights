ebce703b2f6f6567a0b47fca5b3a00b002189232
Arlisson N. Costa <arlisson314@gmail.com>
2022-12-14 13:49:48 -0500

    refactor: matches_salary_range

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,15 +1,13 @@
 from typing import List, Dict
 from src.insights.jobs import read
 
+indt = "industry"
+
 
 def get_unique_industries(path: str) -> List[str]:
     list_infos = read(path)
-    unique_industries = set()
-    for industry in list_infos:
-        if industry["industry"]:
-            unique_industries.add(industry["industry"])
-    return unique_industries
+    return {industry[indt] for industry in list_infos if industry[indt]}
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    return [job for job in jobs if job["industry"] == industry]
+    return [job for job in jobs if job[indt] == industry]
