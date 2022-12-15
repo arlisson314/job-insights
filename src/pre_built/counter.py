4d2e745dea385302d3f3093990e6168d95ea60d4
Arlisson N. Costa <arlisson314@gmail.com>
2022-12-15 13:13:28 -0500

    refactor : test para a função count_ocurrences

--- src/pre_built/counter.py
+++ src/pre_built/counter.py
@@ -2,4 +2,8 @@ def count_ocurrences(path: str, word: str) -> int:
     file = open(path, "r")
     read_data = file.read()
     word_count = read_data.lower().count(word.lower())
+
     return word_count
+
+
+# print("TESTE ====> ", count_ocurrences("data/jobs.csv", "javascript"))
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,13 +1,12 @@
 from src.pre_built.counter import count_ocurrences
-import pytest
+
+# import pytest
 
 
 def test_counter():
+    assert (count_ocurrences("data/jobs.csv", "javascript")) == 122
     assert (count_ocurrences("data/jobs.csv", "python")) == 1639
-
-    assert (count_ocurrences("data/jobs.csv", "javascript")) != 22
-    assert (count_ocurrences("data/jobs.csv", "python")) != 140
-
+    assert (count_ocurrences("data/jobs.csv", "python")) != 10
     assert (count_ocurrences("data/jobs.csv", "Comed!ante")) == 0
-    with pytest.raises(FileNotFoundError):
-        count_ocurrences("non-existent-file.txt", "Arilsson")
+    # with pytest.raises(FileNotFoundError):
+    #     count_ocurrences("non-existent-file.txt", "Arilsson")
