81bcfa9aa7ad6e1192aaba1e1efc89a042623d92
Arlisson N. Costa <arlisson314@gmail.com>
2022-12-19 18:49:51 -0500

    feat: implementa função de teste test_sort_by_criteria

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,64 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+
+jobs = [
+    {
+        "date_posted": "2022-12-19",
+        "max_salary": 5000,
+        "min_salary": 1500,
+    },
+    {
+        "date_posted": "2022-12-15",
+        "max_salary": 15000,
+        "min_salary": 100,
+    },
+    {
+        "date_posted": "2022-12-10",
+        "max_salary": 3000,
+        "min_salary": 1000,
+    },
+]
+
+jobs_sorted_by_max_salary = [
+    {
+        "date_posted": "2022-12-15",
+        "max_salary": 15000,
+        "min_salary": 100,
+    },
+    {
+        "date_posted": "2022-12-19",
+        "max_salary": 5000,
+        "min_salary": 1500,
+    },
+    {
+        "date_posted": "2022-12-10",
+        "max_salary": 3000,
+        "min_salary": 1000,
+    },
+]
+
+jobs_sorted_by_min_salary = [
+    {
+        "date_posted": "2022-12-15",
+        "max_salary": 15000,
+        "min_salary": 100,
+    },
+    {
+        "date_posted": "2022-12-10",
+        "max_salary": 3000,
+        "min_salary": 1000,
+    },
+    {
+        "date_posted": "2022-12-19",
+        "max_salary": 5000,
+        "min_salary": 1500,
+    },
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(jobs, "max_salary")
+    assert (jobs) == jobs_sorted_by_max_salary
+
+    sort_by(jobs, "min_salary")
+    assert (jobs) == jobs_sorted_by_min_salary
+    assert (jobs) != jobs_sorted_by_max_salary
